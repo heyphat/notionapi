a680027b56ed95c72852d36c18fdf959a7be48bb
Phat Huynh <phat@kakao.com>
2022-02-24 06:26:31 +0900

    Update path

--- database_test.go
+++ database_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/jomei/notionapi"
+	"github.com/itsphat/notionapi"
 )
 
 func TestDatabaseClient(t *testing.T) {
--- object_test.go
+++ object_test.go
@@ -5,7 +5,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/jomei/notionapi"
+	"github.com/itsphat/notionapi"
 )
 
 func TestDate(t *testing.T) {
